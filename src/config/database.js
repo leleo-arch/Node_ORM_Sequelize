e2f1d3217acea81da5c2fb951a97ed319fb90592
leleo-arch <leo_xdplay@hotmail.com>
2024-04-01 20:03:48 -0300

    First-Commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  username: "postgres",
+  host: "localhost",
+  password: "postgres",
+  database: "code_trattoria",
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
