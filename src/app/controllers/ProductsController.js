b5cf5a6cd9dc9e19fd87e16203c8f0dbb6d641db
leleo-arch <leo_xdplay@hotmail.com>
2024-06-28 22:47:06 -0300

    commit

--- src/app/controllers/ProductsController.js
+++ src/app/controllers/ProductsController.js
@@ -7,40 +7,31 @@ class SessionProductController {
       name: Yup.string().required(),
       price: Yup.number().required(),
       category: Yup.string().required(),
-
     });
- 
-try{
-        await schema.validateSync(request.body, {abortEarly: false})
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: true });
     } catch (err) {
-        return response.status(400).json({error: err.errors })
+      return response.status(400).json({ error: err.errors });
     }
 
-const {filename: path} = request.file;
-const {name, price, category} = request.body
+    const { filename: path } = request.file;
+    const { name, price, category } = request.body;
 
-const product = Products.create({
-  name,
-  price,
-  category,
-  path,
-});
+    const product = Products.create({
+      name,
+      price,
+      category,
+      path,
+    });
 
-    return response.status(201).json(product)
+    return response.status(201).json(product);
+  }
+
+  async index(request, response) {
+    const products = await Products.findAll();
+    console.log(request);
+    return response.status(201).json(products);
+  }
 }
-
-async index(request, response) {
-  const products = await Products.findAll()
-  console.log(request)
-  return response.status(201).json(products)
-
-}
-
-}
-export default new SessionProductController ();
-
-
-
-
-
-
+export default new SessionProductController();
